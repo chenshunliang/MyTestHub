fe701765f26159ec95e865a16908f2d1c4abc577
chenshunliang <imchensl@163.com>
2014-08-08 17:56:14 +0800

    web api入门

---
+++ IMChat/WebApi/Controllers/ProductController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    public class ProductController : ApiController
+    {
+        Product[] pros = new Product[] { 
+        new Product(){ ID=1, Name="手机", Price=1200.00m},
+        new Product(){ID=2,Name="笔记本", Price=3500.00m},
+        new Product(){ID=3,Name="汽车", Price=120000.00m}
+        };
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return pros;
+        }
+
+        public string GetProNameById(int id)
+        {
+            if (id >= pros.Length || id < 0)
+                return "无此产品";
+            else
+            {
+                return pros[id].Name;
+            }
+        }
+    }
+}
