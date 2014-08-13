030cc5d5673018d94eb82c41fd39b29a6a5b216e
chenshunliang <imchensl@163.com>
2014-08-13 16:51:23 +0800

    web api

--- IMChat/WebApiClient/Program.cs
+++ IMChat/WebApiClient/Program.cs
@@ -12,10 +12,64 @@ namespace WebApiClient
     {
         static void Main(string[] args)
         {
+
+            //client配置
+            //WebRequestHandler handler = new WebRequestHandler()
+            //{
+            //    AllowAutoRedirect = false,   //允许自动重连接
+            //    UseProxy = false    //使用代理
+            //};
+            //HttpClient client = new HttpClient(handler);
+
+
             HttpClient client = new HttpClient();
             client.BaseAddress = new Uri("http://localhost:2376/");
             client.DefaultRequestHeaders.Accept.Add(
                 new MediaTypeWithQualityHeaderValue("application/json"));
+
+            HttpResponseMessage response = client.GetAsync("api/product/1").Result;
+            //if (response.IsSuccessStatusCode)
+            //{
+            //    var products = response.Content.ReadAsAsync<Product>().Result;
+            //    //foreach (var p in products)
+            //    //{
+            //    //    Console.WriteLine("{0}\t{1};\t{2}", p.Name, p.Price, p.ID);
+            //    //}
+
+            //    Console.WriteLine("{0}\t{1};\t{2}", products.Name, products.Price, products.ID);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("{0} ({1})", (int)response.StatusCode, response.ReasonPhrase);
+            //}
+
+
+            //HTTP Post请求
+            try
+            {
+                var gizmo = new Product() { Name = "Gizmo", Price = 100, ID = 1 };
+                Uri gizmoUri = null;
+
+                response = client.PostAsJsonAsync("api/products", gizmo).Result;
+                //如果状态码不是200~299  抛异常
+                response.EnsureSuccessStatusCode();
+                //IsSuccessStatusCode 200~299 为true
+                if (response.IsSuccessStatusCode)
+                {
+                    gizmoUri = response.Headers.Location;
+                }
+                else
+                {
+                    Console.WriteLine("{0} ({1})", (int)response.StatusCode, response.ReasonPhrase);
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.ReadKey();
+            }
+            Console.ReadKey();
         }
     }
 }
