488f18090d9c8e929cb9e563bf88311fc917db88
chenshunliang <chenshunliang@yiche.com>
2015-06-12 16:21:00 +0800

    添加消息

---
+++ IMChat/WeiXin/Messages/ImgMessage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WeiXin.Messages
+{
+    /// <summary>
+    /// 图片消息
+    /// </summary>
+    public class ImgMessage : BaseMessage
+    {
+        /// <summary>
+        /// 图片路径
+        /// </summary>
+        public string PicUrl { get; set; }
+        /// <summary>
+        /// 消息id，64位整型
+        /// </summary>
+        public string MsgId { get; set; }
+        /// <summary>
+        /// 媒体ID
+        /// </summary>
+        public string MediaId { get; set; }
+    }
+}
---
+++ IMChat/WeiXin/Messages/TextMessage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WeiXin.Messages
+{
+    /// <summary>
+    /// 文本消息
+    /// </summary>
+    public class TextMessage : BaseMessage
+    {
+        /// <summary>
+        /// 消息内容
+        /// </summary>
+        public string Content { get; set; }
+        /// <summary>
+        /// 消息id，64位整型
+        /// </summary>
+        public string MsgId { get; set; }
+    }
+}
